f58ce4b3d548da23f83a2baecda5bfff9acdc93c
Cary Pennington <carypenn@gmail.com>
2015-08-07 17:42:48 -0400

    updated for issues

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -11,7 +11,7 @@ class PatientsController < ApplicationController
       @patients = Patient.search(params[:query], load: true)
       @patients = @patients.sort_by { |p| p.last_name }
     else
-      @patients = Patient.order(created_at: :desc)
+      @patients = Patient.order(last_name: :desc)
     end
   end
 
